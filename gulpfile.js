453aff9a920c32ccbd141e38d8861a716fb3fa9c
Asuki <changes729@163.com>
2020-12-31 08:50:41 +0800

    support lessc translate less to css.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require("gulp");
 var markdown = require("gulp-markdown-it");
+var less = require("gulp-less");
 
 gulp.task("markdown", function () {
-  return gulp.src("doc/**/*.md").pipe(markdown()).pipe(gulp.dest("dist/"));
+  return gulp.src("doc/**/*.md").pipe(markdown()).pipe(gulp.dest("dist/html/"));
+});
+
+gulp.task("less", function (cb) {
+  gulp.src("less/**/*.less").pipe(less()).pipe(gulp.dest("dist/css/"));
+  cb();
 });
 
 gulp.task("default", function () {
-  return gulp.watch("doc/**/*.md", gulp.series(["markdown"]));
+  gulp.watch("less/**/*.less", gulp.series("less"));
+  gulp.watch("doc/**/*.md", gulp.series(["markdown"]));
 });
